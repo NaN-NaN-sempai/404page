58df213553ad1769b8707958906bbc5585d1d0a3
NaN-NaN-sempai <lubizinho-2001@hotmail.com>
2017-10-13 02:01:31 -0300

    update 06

--- 404things/js/JS.js
+++ 404things/js/JS.js
@@ -1,3 +1,13 @@
+document.getElementById("btnb").onload = lala();
+
+
+function lala(){
+	btna();
+	btnb();
+	btnc();
+}
+	
+
 (function(w, d, n, s, t) {
     w[n] = w[n] || [];
     w[n].push(function() {
@@ -25,3 +35,69 @@
     s.async = true;
     t.parentNode.insertBefore(s, t);
 })(window, document, "yandex_context_callbacks");
+
+function btna() {
+setInterval( function(){ 
+	var str = document.getElementById("btna").innerHTML;
+
+var rl = str.charAt(Math.floor((Math.random() * str.length) + 1));
+    var test = str.replace(rl, rl.toUpperCase());
+
+    document.getElementById("btna").innerHTML = test;
+    }, 40);
+    
+
+    setInterval( function(){
+    var str = document.getElementById("btna").innerHTML;
+
+var rl = str.charAt(Math.floor((Math.random() * str.length) + 1));
+    var test = str.replace(rl, rl.toLowerCase());
+
+    document.getElementById("btna").innerHTML = test;
+
+    }, 70);
+}
+
+function btnb() {
+setInterval( function(){ 
+	var str = document.getElementById("btnb").innerHTML;
+
+var rl = str.charAt(Math.floor((Math.random() * str.length) + 1));
+    var test = str.replace(rl, rl.toUpperCase());
+
+    document.getElementById("btnb").innerHTML = test;
+    }, 40);
+    
+
+    setInterval( function(){
+    var str = document.getElementById("btnb").innerHTML;
+
+var rl = str.charAt(Math.floor((Math.random() * str.length) + 1));
+    var test = str.replace(rl, rl.toLowerCase());
+
+    document.getElementById("btnb").innerHTML = test;
+
+    }, 70);
+}
+
+function btnc() {
+setInterval( function(){ 
+	var str = document.getElementById("btnc").innerHTML;
+
+var rl = str.charAt(Math.floor((Math.random() * str.length) + 1));
+    var test = str.replace(rl, rl.toUpperCase());
+
+    document.getElementById("btnc").innerHTML = test;
+    }, 40);
+    
+
+    setInterval( function(){
+    var str = document.getElementById("btnc").innerHTML;
+
+var rl = str.charAt(Math.floor((Math.random() * str.length) + 1));
+    var test = str.replace(rl, rl.toLowerCase());
+
+    document.getElementById("btnc").innerHTML = test;
+
+    }, 70);
+}
